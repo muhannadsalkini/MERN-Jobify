d6c00068ba4d1ad6d4257a91de2786da3b2da164
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-09-05 12:47:31 +0300

    Error Page

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,29 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Link, useRouteError } from "react-router-dom";
+import img from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 
 const Error = () => {
-  const error = useRouteError(); // Find more about the error
+  const error = useRouteError();
   console.log(error);
-
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh! page not found</h3>
+          <p>We can't seem to find the page you're looking for</p>
+          <Link to="/dashboard">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
-    <>
-      <h1>Error</h1>
-      <Link to={"/"}>Back Home</Link>
-    </>
+    <Wrapper>
+      <div>
+        <h3>something went wrong</h3>
+      </div>
+    </Wrapper>
   );
 };
 
