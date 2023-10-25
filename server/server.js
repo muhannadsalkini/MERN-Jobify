602041c4aa16350091745ac20d959cf89a64fba1
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-10-25 13:42:10 +0300

    Setup Morgan & Dotenv

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 import express from "express";
+import morgan from "morgan"; // Logging HTTP requests in web applications
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
+const port = process.env.PORT || 5100;
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5100, () => {
-  console.log("server running....");
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ mesaage: "data receved", data: req.body });
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}...`);
 });
