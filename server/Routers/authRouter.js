b3fc72587f32bd524d48fc9d51921bf6222b7750
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-11-06 23:48:57 +0300

    Added Login Logic

--- server/Routers/authRouter.js
+++ server/Routers/authRouter.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { register, login } from "../controllers/authController.js";
-import { validateRegisterInput } from "../middleware/validationMiddleware.js";
+import {
+  validateRegisterInput,
+  validateLoginInput,
+} from "../middleware/validationMiddleware.js";
 
 const router = Router();
 
 router.route("/register").post(validateRegisterInput, register);
-router.route("/login").post(login);
+router.route("/login").post(validateLoginInput, login);
 
 export default router;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,9 @@
 import User from "../models/UserModel.js";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { hashPassword, comparePassword } from "../utils/passwordUtils.js";
 
 // Register
 export const register = async (req, res) => {
-  const hashedPassword = hashPassword(req.body.password);
+  const hashedPassword = await hashPassword(req.body.password);
   req.body.password = hashedPassword;
   try {
     const user = await User.create(req.body);
@@ -17,6 +17,14 @@ export const register = async (req, res) => {
 // Login
 export const login = async (req, res) => {
   try {
+    const user = await User.findOne({ email: req.body.email });
+    const isValidUser =
+      user && (await comparePassword(req.body.password, user.password));
+
+    if (!isValidUser)
+      return res.status(401).json({ msg: "invalid credentials" });
+
+    res.send("login route");
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "server error" });
