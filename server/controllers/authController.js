c7dd086e7ff30fdbcdab1360380e3e1145483ca6
MUHANNAD SALKINI <mohanad.salkini@gmail.com>
2023-11-06 23:13:40 +0300

    Hash Password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,14 @@
 import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
 
 // Register
 export const register = async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
   try {
     const user = await User.create(req.body);
-    res.status(201).json({ user });
+    res.status(201).json({ mag: "user created" });
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "server error" });
